59bbf6b0335794361c66861eba6e99d0fb5e61d8
Arshdeep Singh <arshdeep0220@gmail.com>
2022-03-12 22:57:07 +0530

    login

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
 const multer = require('multer');
-var fs = require('fs');
-var Grid = require('gridfs-stream');
+const encrypt = require("mongoose-encryption");
 const { log } = require('console');
-Grid.mongo = mongoose.mongo;
-
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 app.set('view engine','ejs');
+
+let signedIn = false;
 
 const Storage = multer.diskStorage({
     destination:"./public/Uploads/",
@@ -39,9 +39,59 @@ const tuteSchema = {
 }
 const Tute = mongoose.model('Tute',tuteSchema);
 
+const userSchema =new mongoose.Schema({
+    username:String,
+    password:String
+});
+userSchema.plugin(encrypt,{secret:process.env.PASS,encryptedFields:['password']});
+
+const User = mongoose.model('User',userSchema);
+
+
 
 app.get("/",function(req,res){
-    res.render("home");
+    res.render("login");
+});
+
+app.post("/login",function(req,res){
+    const loginUser = req.body.username;
+    const pass = req.body.pass;
+
+    User.findOne({username:loginUser},function(err,result){
+        if(!err){
+            
+            if(result.password === pass){
+                signedIn = true;
+                res.redirect("/home");
+            }
+        }
+    });
+});
+
+app.get("/signup",function(req,res){
+    res.render("signup");
+});
+
+app.post("/signup",function(req,res){
+    const user = req.body.username;
+    const pass = req.body.pass;
+
+    const newUser = new User({
+        username:user,
+        password:pass
+    });
+    newUser.save();
+    signedIn = true;
+    res.redirect("/home");
+});
+
+app.get("/home",function(req,res){
+    if(signedIn === true){
+        res.render("home");
+    }
+    else{
+        alert("Not Logged In");
+    }
 });
 
 app.get("/video",function(req,res){
@@ -49,22 +99,33 @@ app.get("/video",function(req,res){
 });
 
 app.get("/blog",function(req,res){
-
-    Blog.find(function(err,result)
+    if(signedIn == true)
+    {Blog.find(function(err,result1)
     {
         if(!err){
-            res.render("blog",{blogs:result});
+            res.render("blog",{blogs:result1});
         }
     });
+}
+else{
+    alert("Not Logged In");
+    res.redirect("/")
+}
     
 });
 
 app.get("/guides",function(req,res){
+    if(signedIn===true){
     Tute.find(function(err,result){
         if(!err){
             res.render("guides",{records:result});
         }
     });
+    }
+    else{
+        alert("Not logged in");
+        res.redirect("/");
+    }
 });
 
 app.post("/guides",upload,function(req,res){
@@ -95,12 +156,14 @@ app.post("/blog",function(req,res){
 app.get("/blog/:customName",function(req,res){
     const blogId = req.params.customName;
 
-    Blog.findOne({_id:blogId},function(err,result){
+    Blog.findOne({_id:blogId},function(err,result3){
         if(!err){
-            res.render("post",{oneTitle:result.title,oneContent:result.content});
+            res.render("post",{oneTitle:result3.title,oneContent:result3.content});
         }
     });
 });
+
+
 
 app.listen(3000,function(err)
 {
